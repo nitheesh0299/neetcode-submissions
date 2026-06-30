2187f005fa7a490618432ded3f921a5c9658452c
nitheesh0299 <76263836+nitheesh0299@users.noreply.github.com>
2026-07-01 00:12:53 +0530

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> adj = new HashMap<>();
+        for(List<String> ticket:tickets){
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            if(!adj.containsKey(from)){
+                adj.put(from, new PriorityQueue<String>());
+            }
+            adj.get(from).add(to);
+        }
+
+        Stack<String> stack = new Stack<>();
+        List<String> results = new ArrayList<>();
+
+        stack.add("JFK");
+
+        while(!stack.isEmpty()){
+            String curr = stack.peek();
+
+            if(adj.containsKey(curr) && !adj.get(curr).isEmpty()){
+                stack.push(adj.get(curr).poll());
+            }else{
+                results.add(stack.pop());
+            }
+        }
+        Collections.reverse(results);
+        return results;
+    }
+}
